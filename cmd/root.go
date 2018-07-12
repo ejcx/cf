1e520d6dcbf8dc74a5cfe8544d1a4a492abe170f
Evan Johnson <e@ejj.io>
2018-07-12 14:01:35 -0700

    Die if no creds.
    This isn't the behavior we want in the long term but it is okay
    for now.

--- cmd/root.go
+++ cmd/root.go
@@ -2,8 +2,10 @@ package cmd
 
 import (
 	"fmt"
+	"log"
 	"os"
 
+	"github.com/ejcx/cf/lib"
 	"github.com/spf13/cobra"
 )
 
@@ -18,26 +20,12 @@ var RootCmd = &cobra.Command{
 	Short: "A CLI for interacting with Cloudflare's V4 API",
 }
 
-func loadConfiguration() {
-
-}
-
 func Execute() {
-	var (
-		cfSecret string
-		cfEmail  string
-		envSet   bool
-	)
-	// If we have environment variables set then we should not attempt to
-	// load the $HOME/.cf/credentials file.
-	if secret, ok := os.LookupEnv("CF_API_KEY"); ok {
-		cfSecret = secret
-		envSet = true
+	err := lib.DefaultCredentialProvider.ConfigureEnvironment()
+	if err != nil {
+		log.Fatalf("No set of credentials to use: %s", err)
 	}
-	if email, ok := ok.LookupEnv("CF_API_EMAIL"); ok {
-		cfEmail = email
-		envSet = true
-	}
+
 	if err := RootCmd.Execute(); err != nil {
 		fmt.Println(err)
 		os.Exit(1)
--- lib/creds.go
+++ lib/creds.go
@@ -8,7 +8,7 @@ import (
 )
 
 var (
-	Default = &CredProvider{}
+	DefaultCredentialProvider = &CredProvider{}
 )
 
 type CredProvider struct {
